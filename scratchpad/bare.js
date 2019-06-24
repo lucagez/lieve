c45424d746d71fe7d9c6b9ddcb4838accf594089
lucagez <lucagesmundo@yahoo.it>
2019-06-24 12:30:31 +0200

    removed scratches => inside bench

--- scratchpad/bare.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.setHeader('content-type', 'application/json; charset=utf-8');
-  res.end('hello');
-});
-
-server.listen(3000, () => console.log('listening'));
--- scratchpad/fastify.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const fastify = require('fastify')()
-
-fastify.get('/', function (req, reply) {
-  reply.send('hello')
-})
-
-fastify.listen(3000)
