2901461bab97c010dd54cdf5ad7c2c31d7a082e1
lucagez <lucagesmundo@yahoo.it>
2019-06-25 18:50:48 +0200

    turbo-http compatibility

--- src/_utils.js
+++ src/_utils.js
@@ -2,9 +2,8 @@ const deSlash = str => str.replace(/\//g, '');
 
 const sendNotFound = (res, messageObj) => {
   const { type, message } = messageObj;
-  res.writeHead(404, {
-    'Content-Type': type,
-  });
+  res.statusCode = 404;
+  res.setHeader('Content-Type', type);
   res.end(message);
 };
 
